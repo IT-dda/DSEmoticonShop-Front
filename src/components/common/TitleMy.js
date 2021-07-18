99f245bb315b279e38390dc5e84160bb38ec8264
seoyeonko <seoyeon99o@naver.com>
2021-07-19 03:37:04 +0900

    Design: 마이페이지 좋아요 UI

--- src/components/common/TitleMy.js
+++ src/components/common/TitleMy.js
@@ -11,13 +11,23 @@ const TitleMyBlock = styled.div`
     line-height: 27px;
     font-weight: bold;
   }
+
+  .tit_num {
+    display: inline-block;
+    margin-left: 4px;
+    font-size: 18px;
+    line-height: 27px;
+    font-weight: bold;
+    color: #fa4637;
+  }
 `;
 
-const TitleMy = ({ title }) => {
+const TitleMy = ({ title, count }) => {
   return (
     <TitleMyBlock>
       <h4 className="tit_mypage">
         <span className="txt_tit">{title}</span>
+        <span className="tit_num">{count}</span>
       </h4>
     </TitleMyBlock>
   );
--- src/pages/mypage/LikesPage.js
+++ src/pages/mypage/LikesPage.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
+import TitleMy from '../../components/common/TitleMy';
+import LikesItem from './mypageList/LikesItem';
+
+const LikesPageBlock = styled.div`
+  margin: 0 20%;
+
+  div {
+    margin: 0.5rem;
+  }
+`;
 
 const LikesPage = () => {
-  return <div>좋아요 리스트 보이기</div>;
+  return (
+    <LikesPageBlock>
+      <TitleMy title="좋아요" count="6" />
+      <hr />
+      <div>
+        <LikesItem />
+        <LikesItem />
+        <LikesItem />
+        <LikesItem />
+        <LikesItem />
+        <LikesItem />
+      </div>
+    </LikesPageBlock>
+  );
 };
 
 export default LikesPage;
