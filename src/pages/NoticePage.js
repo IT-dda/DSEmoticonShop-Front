9ba187284f9ee59f7029eb718d3d3c9b844e80a7
seoyeonko <seoyeon99o@naver.com>
2021-07-24 22:53:11 +0900

    Design: 새소식 UI

--- src/pages/NoticePage.js
+++ src/pages/NoticePage.js
@@ -2,16 +2,18 @@
 import React from 'react';
 import Contents from '../components/common/Contents';
 import styled from 'styled-components';
+import Header from '../components/common/Header';
 
 const NoticePageBlock = styled.div`
   width: 100%;
-  height: 100%;
+  min-height: 100vh;
   background: #fafafa;
 `;
 
 const NoticePage = () => {
   return (
     <NoticePageBlock>
+      <Header />
       <Contents title={'새소식'} />
     </NoticePageBlock>
   );
