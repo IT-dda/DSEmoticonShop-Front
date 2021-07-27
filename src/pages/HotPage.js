93aac6e82598430070ff8493768421038ffe8f8e
seoyeonko <seoyeon99o@naver.com>
2021-07-28 00:12:30 +0900

    Design: 홈 footer 추가

--- src/pages/HotPage.js
+++ src/pages/HotPage.js
@@ -3,10 +3,14 @@ import Banner from '../components/common/Banner';
 import HotItemList from '../components/hot/HotItemList';
 import styled from 'styled-components';
 import Header from '../components/common/Header';
+import Footer from '../components/common/Footer';
 
 const Wrapper = styled.div`
   display: flex;
   justify-content: center;
+
+  /* viewport_height - Hader_height - Banner_height */
+  min-height: calc(100vh - 110px - 170px);
 `;
 
 const HotPage = () => {
@@ -17,6 +21,7 @@ const HotPage = () => {
       <Wrapper>
         <HotItemList></HotItemList>
       </Wrapper>
+      <Footer />
     </>
   );
 };
--- src/pages/StylePage.js
+++ src/pages/StylePage.js
@@ -3,11 +3,15 @@ import StyleBox from '../components/style/StyleBox';
 import styled from 'styled-components';
 import Banner from '../components/common/Banner';
 import Header from '../components/common/Header';
+import Footer from '../components/common/Footer';
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  /* viewport_height - Hader_height - Banner_height */
+  min-height: calc(100vh - 110px - 170px);
 `;
 
 const StylePage = () => {
@@ -21,6 +25,7 @@ const StylePage = () => {
         <StyleBox name={'귀여운'} id={3} />
         <StyleBox name={'동물'} id={4} />
       </Wrapper>
+      <Footer />
     </>
   );
 };
