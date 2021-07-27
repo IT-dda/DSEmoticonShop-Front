f4a48deba9c4033750a6b6a7b1ea5ffcfa60a77a
seoyeonko <seoyeon99o@naver.com>
2021-07-27 23:56:44 +0900

    Design: 마이페이지 Footer 추가

--- src/pages/FaQPage.js
+++ src/pages/FaQPage.js
@@ -2,19 +2,29 @@ import React from 'react';
 import Contents from '../components/common/Contents';
 import styled from 'styled-components';
 import Header from '../components/common/Header';
+import Footer from '../components/common/Footer';
 
 const FaQPageBlock = styled.div`
   width: 100%;
-  min-height: 100vh;
   background: #fafafa;
+
+  /* viewport_height - Hader_height - MyBanner_height - Foter_height */
+  min-height: calc(100vh - 110px - 190px - 50px);
+`;
+
+const FaQFooter = styled(Footer)`
+  height: 50px;
 `;
 
 const FaQPage = () => {
   return (
-    <FaQPageBlock>
+    <>
       <Header />
-      <Contents title="자주묻는 질문" />
-    </FaQPageBlock>
+      <FaQPageBlock>
+        <Contents title="자주묻는 질문" />
+      </FaQPageBlock>
+      <FaQFooter />
+    </>
   );
 };
 
--- src/pages/NoticePage.js
+++ src/pages/NoticePage.js
@@ -3,19 +3,29 @@ import React from 'react';
 import Contents from '../components/common/Contents';
 import styled from 'styled-components';
 import Header from '../components/common/Header';
+import Footer from '../components/common/Footer';
 
 const NoticePageBlock = styled.div`
   width: 100%;
-  min-height: 100vh;
   background: #fafafa;
+
+  /* viewport_height - Hader_height - MyBanner_height - Foter_height */
+  min-height: calc(100vh - 110px - 190px - 50px);
+`;
+
+const NoticeFooter = styled(Footer)`
+  height: 50px;
 `;
 
 const NoticePage = () => {
   return (
-    <NoticePageBlock>
+    <>
       <Header />
-      <Contents title="새소식" />
-    </NoticePageBlock>
+      <NoticePageBlock>
+        <Contents title="새소식" />
+      </NoticePageBlock>
+      <NoticeFooter />
+    </>
   );
 };
 
